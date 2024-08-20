5a6a466b2ebea19f29a5f15bc4b415f1165af9da
officefish <indicogames@yandex.ru>
2024-08-20 07:08:40 +0300

    install snackbar

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,14 @@
 import { FC, PropsWithChildren } from "react"
 import { SiteProvider } from "./store"
+import { SnackbarProvider } from "notistack"
 
 const Providers: FC <PropsWithChildren> = ({ children }) => {
-    return <SiteProvider>
-        {children}
-    </SiteProvider>
+    return (
+        <SnackbarProvider maxSnack={3}>
+            <SiteProvider>
+                {children}
+            </SiteProvider>
+        </SnackbarProvider>
+    )
 }
 export default Providers
