c7da9e867f77b674b0359b8e175a13b1f4349f72
officefish <indicogames@yandex.ru>
2024-09-10 11:30:31 +0300

    ngrok server update

--- src/services/api/index.tsx
+++ src/services/api/index.tsx
@@ -5,7 +5,7 @@ import Auth from './auth';
 let activeRequests = 0;
 
 //const apiURL = import.meta.env.VITE_API_URL || 'http://188.68.221.24/api/v1'
-const apiURL = import.meta.env.VITE_API_URL || 'https://93bd-5-18-176-212.ngrok-free.app/api/v1'
+const apiURL = import.meta.env.VITE_API_URL || ' https://bc76-5-18-176-212.ngrok-free.app/api/v1'
 
 export default async function apiFetch(
     url: string, 
