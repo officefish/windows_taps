e48aad1af6113d37d1b4b213fa9e88a4876f0f38
officefish <indicogames@yandex.ru>
2024-09-15 01:21:21 +0300

    friends useEffect fix

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -37,7 +37,7 @@ const Friends: FC = () => {
 
     console.log('refferals:', refferals)
     
-  }, [referralsPage, getRefferals, referralsTotal, referralsCode, refferals])
+  }, [])
 
   //const position = useBackgroundMover(7); // Adjust multiplier as needed
 
